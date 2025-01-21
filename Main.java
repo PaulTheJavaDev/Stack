7f7d714677f9d41eabe55a6e694125b66ab38e05
Paul <Paul-Ludwig.Simon@t-online.de>
2025-01-21 21:54:00 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //stack = LIFO data structure. Last-In First-Out
+        //stores objects into a sort of "vertical tower"
+        //push() to add to the top
+        //pop() to remove from the top
+
+        // uses of stacks?
+        // 1. undo/red features in text editors
+        // 2. moving back/forward through browser history
+        // 3. backtracking algorithms (maze, file directories)
+        // 4. calling functions (call stack)
+
+        
         
     }
 
